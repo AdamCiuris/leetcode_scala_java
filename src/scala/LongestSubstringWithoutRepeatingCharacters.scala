c934f6ed11afdbd7491e8269d5c3af1def62c6ca
Adam Ciuris <adamciuris@gmail.com>
2022-06-21 18:20:41 -0500

    scala

--- src/scala/LongestSubstringWithoutRepeatingCharacters.scala
+++ src/scala/LongestSubstringWithoutRepeatingCharacters.scala
@@ -6,28 +6,45 @@ import scala.annotation.tailrec
 * Adam Ciuris
 * Scala 3
 * Solution to leetcode longest substring without repeating characters
-*
+* sliding window pattern
 */
 object LongestSubstringWithoutRepeatingCharacters extends App {
   @tailrec
-  def lengthOfLongestSubstring(s: String, start: Int = 0, end: Int = 0, map: Map[Char, Int]): Int = {
-
-    val w = s.charAt(start)
-
-    val _map: Map[Char, Int] = map + (w -> 1)
-
-    val _start = start+1
-//    val _end = s
-    println(map)
-    println(_map)
-    if start != end-1 then
-    lengthOfLongestSubstring(s,_start, end,_map) else 9
+  def lengthOfLongestSubstring(s: String, start: Int = 0, end: Int = 0, map: Map[Char, Int], longest: Int = 0): Int = {
+    if (end <= s.length-1) {
+      val w = s.charAt(start)
+      val charAt = s.charAt(end)
+      map.get(charAt) match {
+        case Some(e) =>
+          val _start = e + 1 // move window to one position higher than index of prev char
+          val _end = end + 1
+          val _map = map.updated(charAt, end) // peculiar syntax change
+          println(_end- start)
+          lengthOfLongestSubstring(s, _start, _end, _map, longest)
+        case None =>
+          val _end = end + 1
+          val _map: Map[Char, Int] = map + (charAt -> end)
+          println(_end- start)
+          val _longest = if longest < _end-start then end-start else 0
+          lengthOfLongestSubstring(s, start, _end, _map, _longest)
+      } // a b c d a e f
+    }
+    else
+      longest+1 // add one to account for index 0 stuff
 
   }
 
-  val myString = "pwwkew"
-  val pls: Map[Char, Int] = Map('p' -> 0)
 
-  val yes = lengthOfLongestSubstring(myString, end=myString.length, map=pls)
+  val myString1 = "abcabcbb" // 3
+  val myString2 = "bbbbb" // 1
+  val myString3 = "pwwkew" // 3
+  val pls: Map[Char, Int] = Map()
+
+  val test1 = lengthOfLongestSubstring(myString1, map=pls)
+  val test2 = lengthOfLongestSubstring(myString2, map=pls)
+  val test3= lengthOfLongestSubstring(myString3, map=pls)
+  println(myString1 +" result: " + test1)
+  println(myString2 +" result: " + test2)
+  println(myString3 +" result: " + test3)
 
 }
